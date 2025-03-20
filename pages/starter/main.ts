679d33db372ba57bc1f97b79e4b6eadd2f0c0a72
42arch <rend42@163.com>
2025-03-20 10:39:03 +0800

    update page

--- pages/starter/main.ts
+++ pages/starter/main.ts
@@ -2,22 +2,33 @@ import {
   ACESFilmicToneMapping,
   AmbientLight,
   BoxGeometry,
+  BufferGeometry,
   Clock,
+  Color,
   DirectionalLight,
   Group,
+  Line,
   LinearSRGBColorSpace,
+  LineBasicMaterial,
   Mesh,
   MeshNormalMaterial,
   PerspectiveCamera,
   Scene,
+  Vector3,
   WebGLRenderer
 } from 'three'
-import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import {
+  Line2,
+  LineGeometry,
+  LineMaterial,
+  OrbitControls
+} from 'three/examples/jsm/Addons.js'
 import { Pane } from 'tweakpane'
 
 class View {
   private width: number
   private height: number
+  private pixelRatio: number
   private canvas: HTMLElement
   private scene: Scene
   private camera: PerspectiveCamera
@@ -34,19 +45,24 @@ class View {
     this.canvas = document.querySelector(element) as HTMLElement
     this.width = window.innerWidth
     this.height = window.innerHeight
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2)
     this.scene = new Scene()
-    this.camera = new PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
+    this.camera = new PerspectiveCamera(
+      75,
+      this.width / this.height,
+      0.001,
+      1000
+    )
     this.camera.position.set(0, 0, 3)
     this.scene.add(this.camera)
     this.renderer = new WebGLRenderer({
       canvas: this.canvas,
       antialias: true
     })
-    this.renderer.toneMapping = ACESFilmicToneMapping
-    this.renderer.toneMappingExposure = 0.9
-    this.renderer.outputColorSpace = LinearSRGBColorSpace
+
     this.renderer.setSize(this.width, this.height)
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    this.renderer.setPixelRatio(this.pixelRatio)
 
     this.controls = new OrbitControls(this.camera, this.canvas)
     this.controls.enableDamping = true
@@ -74,7 +90,7 @@ class View {
       this.camera.updateProjectionMatrix()
 
       this.renderer.setSize(this.width, this.height)
-      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+      this.renderer.setPixelRatio(this.pixelRatio)
     })
   }
 
@@ -109,13 +125,35 @@ class View {
     const mesh = new Mesh(box, material)
     mesh.castShadow = true
     mesh.receiveShadow = true
+    // this.group.add(mesh)
 
-    this.group.add(mesh)
+    const lineMat = new LineBasicMaterial({
+      color: 0xffffff,
+      linewidth: 20
+    })
+    const lineGeom = new BufferGeometry().setFromPoints([
+      new Vector3(-1, 0, 0),
+      new Vector3(0, 1, 0),
+      new Vector3(1, 0, 0)
+    ])
+    const line1 = new Line(lineGeom, lineMat)
+
+    const lineGeom2 = new LineGeometry()
+    lineGeom2.setPositions([-1, 0, 0, 1, 0, 0])
+    const lineMat2 = new LineMaterial({
+      color: new Color('red'),
+      linewidth: 15
+      // wireframe: true
+    })
+    const line2 = new Line2(lineGeom2, lineMat2)
+
+    this.group.add(line1)
+    this.group.add(line2)
   }
 
   addPane() {
     const pane = new Pane({
-      title: 'Parameters'
+      title: 'Starter'
     })
     pane.addBinding(this.params, 'rotate')
   }
